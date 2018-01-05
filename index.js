5961c63f5bb9b5540378fd1f13ff546f0d810e9e
willarevalo <willarevalo.developer@gmail.com>
2018-01-05 13:28:04 -0500

    add lesson 4 module 2

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import React from 'react';
 //React sirve para crear los componentes
 import ReactDom from 'react-dom';
 //Reactdom para renderizar o colocar los componentes en el dom
+import Media from './src/playlist/components/media.js';
+//importo media 
 //console.log("Hola mundo");
 
 //Referencio el elemento en donde estara vivo react
 const app = document.getElementById('app')
 //ReactDom.render(que voy a renderizar(elemento, o componente), donde lo haré(en que parte del dom));
 //Creando un componente para renderizar
-const holaMundo = <h1>Hola Mundo :) </h1>;
-ReactDom.render(holaMundo, app);
+//const holaMundo = <h1>Hola Mundo :) </h1>;
+ReactDom.render(<Media />, app);
 //En este punto ejecuto en cli el comando npm run build:prod
