5252fcdc246f7e9db77ac903f4cf692470712471
willarevalo <willarevalo.developer@gmail.com>
2018-01-05 13:05:01 -0500

    add lesson 1 module 2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo");
+//Importo react y react-dom
+import React from 'react';
+//React sirve para crear los componentes
+import ReactDom from 'react-dom';
+//Reactdom para renderizar o colocar los componentes en el dom
+//console.log("Hola mundo");
+
+//Referencio el elemento en donde estara vivo react
+const app = document.getElementById('app')
+//ReactDom.render(que voy a renderizar(elemento, o componente), donde lo haré(en que parte del dom));
+//Creando un componente para renderizar
+const holaMundo = <h1>Hola Mundo :) </h1>;
+ReactDom.render(holaMundo, app);
+//En este punto ejecuto en cli el comando npm run build:prod
