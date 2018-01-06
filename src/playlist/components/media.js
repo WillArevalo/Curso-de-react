3ad860281e13f3a1e08fbd7a9be4e6018cc077e8
willarevalo <willarevalo.developer@gmail.com>
2018-01-06 13:08:04 -0500

    Con estilos aparte

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,24 +3,12 @@ import React from 'react';
 import './media.css';
 class Media extends React.Component {
 	render(){
-		//Creando estilos inline, con un json
-		const styles = {
-			container: {
-				fontSize: '1rem',
-				backgroundColor: 'f1f1f1',
-				color: '#44546b',
-				cursor: 'pointer',
-				width: 260,
-				border: '2px solid #ff0909',
-				borderRadius: '3%'
-			}
-		}
 		return(
-			<div style={styles.container}>
-				<div>
-					<img src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
-					<h3>¿Por qué aprender React?</h3>
-					<p>Will Arevalo</p>
+			<div className="Media">
+				<div className="Media-cover">
+					<img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
+					<h3 className="Media-title">¿Por qué aprender React?</h3>
+					<p className="Media-author">Will Arevalo</p>
 				</div>
 			</div>
 		)
