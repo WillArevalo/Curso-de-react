7b44fa36f49641f218dc8584182a3416d62537e2
willarevalo <willarevalo.developer@gmail.com>
2018-01-11 23:28:25 -0500

    add lesson 'Composicion de elementos' :baby_chick:

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css';
+import Play from '../../icons/components/play.js';
+import Pause from '../../icons/components/pause.js';
+import Volumen from '../../icons/components/volumen.js';
+import Fullscreen from '../../icons/components/fullscreen.js';
+
 //Cuando el componente no guarda datos o algo similar se puede transformar 
 //a un componente funcional, que solo vive en la funcion
 function Playlist(props) {
@@ -9,6 +14,26 @@ function Playlist(props) {
 		<div className = "Playlist">
 			<h1 className = "Playlist-titulo">{titulo}</h1>
 			<h4 className = "Playlist-descripcion">{descripcion}</h4>
+			<Play
+				size={20}
+				color="#A0AABF"
+				title="Play"
+			/>
+			<Pause
+				size={20}
+				color="#A0AABF"
+				title="Pause"
+			/>
+			<Volumen
+				size={20}
+				color="#A0AABF"
+				title="Volume"
+			/>
+			<Fullscreen
+				size={20}
+				color="#A0AABF"
+				title="Fullscreen"
+			/>
 			<div className = "Playlist-scroller">
 				{
 					lista.map( item => {
