3bf10167c9496ac3f07c113e0aa99fc9ec8a0ba2
willarevalo <willarevalo.developer@gmail.com>
2018-01-22 08:53:51 -0500

    Revert "Modal que se inicia con la pagina React"
    This reverts commit aad8472a5aa8b4dc4d75cc0634a9a18c519b3356.

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -3,36 +3,20 @@ import HomeLayout from '../components/home-layout.js';
 import Categories from '../../categories/components/categories.js';
 import Related from '../components/related.js';
 import ModalContainer from '../../widgets/containers/modal.js';
-import Modal from '../../widgets/components/modal.js';
 
 class Home extends Component {
-	state = {
-		modalVisible: false,
-	}
-	handleOpenModal = () => {
-		//Esto sirve para setear estados
-		this.setState({
-			modalVisible:true,
-		})
-	}
-	//Funcion de handleClick
-	handleCloseModal = (event) => {
-		this.setState({
-			modalVisible: false,
-		})
-	}
 	render(){
 		return (
 			<HomeLayout> 
 				<Related />
-				<Categories categories={this.props.data.categories}/>
-				<Modal>
+				<Categories categories={this.props.data.categories} />
+				<ModalContainer>
 					<h1>
 						Esto es un portal
 					</h1>
-				</Modal>
+				</ModalContainer>
 			</HomeLayout>
-		)//Haciendo condicionales(ternarios) si es true con la && seguria al siguiente comando
+		)
 	}
 }
 
