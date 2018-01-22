6bbc534fc289ffa17842e170acdf4587af720e59
willarevalo <willarevalo.developer@gmail.com>
2018-01-22 12:19:56 -0500

    retomando materialize

--- src/categories/components/category.js
+++ src/categories/components/category.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Playlist from '../../playlist/components/playlist.js';
-import './category.css';
+
+//material
+import Typography from 'material-ui/Typography';
 
 function Category (props) {
 	return (
 		<div className="Category">
-			<p className="Category-description">
-				{props.description}
-			</p>
-			<h2 className="Category-title">
-				{props.title}
-			</h2>
+			<Typography type="headline" gutterBottom>
+					{props.description}
+			</Typography>
+			<Typography type="display1" gutterBottom>
+					{props.title}
+			</Typography>
 			<Playlist 
 				handleOpenModal = {props.handleOpenModal}
 				playlist={props.playlist}
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Media from './media.js';
-import './playlist.css';
+//material
+import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30,
+  },
+  playlist: {
+  	marginLeft: 10,
+  	marginBottom: 100,
+  }
+});
 //Cuando el componente no guarda datos o algo similar se puede transformar 
 //a un componente funcional, que solo vive en la funcion
 function Playlist(props) {
+	const { classes } = props;
 	// const {titulo, descripcion, lista} = props // --- playlist
 	return (
-		<div className = "Playlist">
+		<Grid container className={classes.playlist}>
 			{
 				props.playlist.map((item) =>{
 					return (
@@ -19,8 +32,8 @@ function Playlist(props) {
 					)
 				})
 			}
-		</div>
+		</Grid>
 	)
 }
 
-export default Playlist;
+export default withStyles(styles) (Playlist);
