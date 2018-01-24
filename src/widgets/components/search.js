35f783ee342e874a5de5d09bfc7fcf1745e38eea
willarevalo <willarevalo.developer@gmail.com>
2018-01-24 10:27:59 -0500

    add lesson 26 UI de forms

---
+++ src/widgets/components/search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './search.css';
+// function Search(props){
+// 	return(
+
+// 	)
+// }
+
+const Search = () => (
+	<form action="" className="Search">
+		<input 
+			type="text" 
+			className="Search-input"
+			placeholder="Buscar a un artista, tema o amigo"
+		/>
+	</form>
+)
+
+export default Search;
