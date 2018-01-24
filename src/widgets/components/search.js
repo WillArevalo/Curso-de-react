480442d54fd908169cc406d26afb30c94eb3b87c
willarevalo <willarevalo.developer@gmail.com>
2018-01-24 11:48:46 -0500

    add lesson 27 Referencias a Eventos de HTML y Formularios

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -5,13 +5,20 @@ import './search.css';
 
 // 	)
 // }
-
-const Search = () => (
-	<form action="" className="Search">
+//ref sirve para referenciar un elemento html en react
+const Search = (props) => (
+	<form 
+		onSubmit={props.handleSubmit}
+		className="Search"
+	>
 		<input 
+			ref={props.setRef}
 			type="text" 
 			className="Search-input"
 			placeholder="Buscar a un artista, tema o amigo"
+			name="search"
+			onChange={props.handleChange}
+			value={props.value}
 		/>
 	</form>
 )
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import Search from '../components/search.js'
 
 class SearchContainer extends Component {
+	state = {
+		value: 'Luis Fonsi'
+	}
+	//la funcion handleSubmit evita que la pagina se recargue
+	handleSubmit = event => {
+		event.preventDefault();
+		console.log(this.input.value, 'submit');
+	}
+	//La funcion setInputRef recoje el valor del input
+	setInputRef = element => {
+		this.input = element;
+	}
+	//handleInputChange permite cambiar el estado del valor del input
+	handleInputChange = event => {
+		this.setState({
+			value: event.target.value.replace(' ', '-')
+		})
+	}
 	render(){
 		return(
-			<Search />
+			<Search 
+				handleSubmit={this.handleSubmit}
+				setRef={this.setInputRef}
+				handleChange={this.handleInputChange}
+				value={this.state.value}
+			/>
 		)
 	}
 }
