1b31b525a66d9ef8cd95992bfacb290d84bdc41d
willarevalo <willarevalo.developer@gmail.com>
2018-01-26 11:49:06 -0500

    29 Componentes de Título y Fuente de Vídeo

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout.js';
+import Video from '../components/video.js';
+import Title from '../components/title.js';
 
 class VideoPlayer extends Component{
 	render(){
 		return(
 			<VideoPlayerLayout>
-				<video 
-					controls
-					autoPlay
-					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" />
+				<Title title="Video chido"/>
+				<Video 
+					autoplay={true}
+					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+				/>
 			</VideoPlayerLayout>
 		)
 	}
